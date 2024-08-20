b735cdaf0d43706fc10f3c9cd03dd32ae97a8ec1
PSilyDev <codes.prakhar@gmail.com>
2024-08-20 23:54:25 +0530

    User tiles added in the sidebar

--- src/components/side bar/SideBar.jsx
+++ src/components/side bar/SideBar.jsx
@@ -1,10 +1,11 @@
 import { SideBarContainer } from "./SideBarContainer"
-import { HOME, SHORTS, SUBSCRIPTION } from "./icons.jsx"
+import { HOME, SHORTS, SUBSCRIPTION, HISTORY, PLAYLISTS, WATCHLATER, LIKEDVIDEOS } from "./icons.jsx"
 
 export const SideBar = () => {
 
     return(
         <div className="flex flex-col">
+            {/* main tiles */}
             <button>
                 <SideBarContainer icon={HOME.icon} text={HOME.name} />
             </button>
@@ -13,6 +14,27 @@ export const SideBar = () => {
             </button>
             <button>
                 <SideBarContainer icon={SUBSCRIPTION.icon} text={SUBSCRIPTION.name} />
+            </button>
+            
+            <hr className="self-center w-11/12 border-t border-gray-300 mt-6" />
+
+            {/* you tiles */}
+            <button>
+                <div className= "mt-5 flex justify-start items-center h-20 rounded-3xl hover:bg-gray-200">
+                    <div className="font-semibold text-2xl ml-10">You ></div>
+                </div>
+            </button>
+            <button>
+                <SideBarContainer icon={HISTORY.icon} text={HISTORY.name} />
+            </button>
+            <button>
+                <SideBarContainer icon={PLAYLISTS.icon} text={PLAYLISTS.name} />
+            </button>
+            <button>
+                <SideBarContainer icon={WATCHLATER.icon} text={WATCHLATER.name} />
+            </button>
+            <button>
+                <SideBarContainer icon={LIKEDVIDEOS.icon} text={LIKEDVIDEOS.name} />
             </button>
         </div>
     )
--- src/components/side bar/SideBarContainer.jsx
+++ src/components/side bar/SideBarContainer.jsx
@@ -1,8 +1,8 @@
 export const SideBarContainer = ({icon, text}) => {
     
     return(
-        <div className= "flex justify-start h-20 rounded-3xl hover:bg-gray-200">
-            <div className="flex items-center ml-8 rounded-full bg-red-400 h-14 w-14">{icon}</div>
+        <div className= "flex justify-start items-center h-20 rounded-3xl hover:bg-gray-200">
+            <div className="flex items-center ml-8 rounded-full bg-red-400 h-14 w-14 p-2">{icon}</div>
             <div className="flex items-center ml-20 text-6xl font-light">{text}</div>
         </div>
     )
