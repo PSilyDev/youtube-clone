da2214a2ecdc5dc07d182530cb972efeb1f42ff1
PSilyDev <codes.prakhar@gmail.com>
2024-08-30 20:27:19 +0530

    side bar ui updated

--- src/components/layout/MainLayout.jsx
+++ src/components/layout/MainLayout.jsx
@@ -13,15 +13,19 @@ export const MainLayout = () => {
     return(
         <div>
             {/* <AppBar /> */}
-            <div className="flex flex-row mt-3">
+            <div className="flex mt-3">
                 {/* <button onClick={toggleDrawer(true)}>open</button> */}
                 {/* <Drawer open={open} onClose={toggleDrawer(false)}> */}
                     {/* {DrawerList} */}
-                <div className={`h-screen bg-green-400 p-5 pt-8 relative ${open ? "w-72" : "w-20"} duration-300`}>
+                <div className={`h-screen bg-green-400 p-3 pt-8 relative ${open ? "w-64" : "w-20"}`}>
                     <button className="absolute -right-5 top-1 bg-white rounded-full" onClick={() => setOpen(!open)}>
                         <svg className="w-10 h-10 p-2 hover:bg-gray-200 hover:rounded-full" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="#000000" d="M0 96C0 78.3 14.3 64 32 64l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 128C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 288c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32L32 448c-17.7 0-32-14.3-32-32s14.3-32 32-32l384 0c17.7 0 32 14.3 32 32z"/></svg>
                     </button>
                     <SideBar open={open} />
+                    {/* <div className="mt-4">
+                        <svg className="w-10 h-10 rounded cursor-pointer block float-left mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path fill="#000000" d="M80 160c0-35.3 28.7-64 64-64l32 0c35.3 0 64 28.7 64 64l0 3.6c0 21.8-11.1 42.1-29.4 53.8l-42.2 27.1c-25.2 16.2-40.4 44.1-40.4 74l0 1.4c0 17.7 14.3 32 32 32s32-14.3 32-32l0-1.4c0-8.2 4.2-15.8 11-20.2l42.2-27.1c36.6-23.6 58.8-64.1 58.8-107.7l0-3.6c0-70.7-57.3-128-128-128l-32 0C73.3 32 16 89.3 16 160c0 17.7 14.3 32 32 32s32-14.3 32-32zm80 320a40 40 0 1 0 0-80 40 40 0 1 0 0 80z"/></svg>
+                        <h1 className={`origin-left ${open ? "text-2xl font-medium" : "text-md font-normal"}`}>Home</h1>
+                    </div> */}
                 </div>
                 {/* </Drawer> */}
                 <div className="bg-blue-400 basis-5/6">main layout</div>
--- src/components/side bar/SideBarContainer.jsx
+++ src/components/side bar/SideBarContainer.jsx
@@ -1,8 +1,8 @@
 export const SideBarContainer = ({icon, text, open}) => {
     console.log('open - ', open);
     return(
-        <div className= {`flex items-center h-11 rounded-xl hover:bg-gray-200 ${open ? "justify-start mx-3" : "justify-center mt-2"} duration-300`}>
-            <div className={`flex items-center rounded-full h-8 w-8`}>{icon}</div>
+        <div className= {`flex items-center h-11 rounded-xl hover:bg-gray-200 ${open ? "justify-start mx-3 mt-2" : "justify-center mt-2"}`}>
+            <div className={`flex items-center rounded-full h-8 w-8 p-1 ${open?"ml-3":""}`}>{icon}</div>
             {open?
             (<div className="flex items-center ml-4 text-lg font-semi">{text}</div>)
             : (<div></div>)}
