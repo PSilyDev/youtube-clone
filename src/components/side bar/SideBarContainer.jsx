296e88348615f154584ca1452b039c712daea426
PSilyDev <codes.prakhar@gmail.com>
2024-09-01 20:14:22 +0530

    tooltip done

--- src/components/side bar/SideBarContainer.jsx
+++ src/components/side bar/SideBarContainer.jsx
@@ -1,12 +1,16 @@
 export const SideBarContainer = ({icon, text, open}) => {
     console.log('open - ', open);
     return(
-        <div className= {`flex items-center h-11 rounded-xl hover:bg-gray-200 ${open ? "justify-start mx-3 mt-2" : "justify-center mx-3 mt-2"}`}>
+        <div className= {`relative flex items-center h-11 rounded-xl hover:bg-gray-200 mx-3 mt-2 ${open ? "justify-start" : "justify-center"} group`}>
             <div className={`flex items-center rounded-full h-8 w-8 p-1`}>{icon}</div>
-            <span className="absolute hidden group-hover:inline-block bg-neutral-900 text-white text-xs p-2 whitespace-nowrap rounded top-1/2 -translate-y-1/2 left-[calc(100%+5px)]"></span>
             {open?
             (<div className="flex items-center ml-4 text-lg font-semi">{text}</div>)
             : (<div></div>)}
+            {!open && (
+                <div className="absolute left-full ml-3 bg-white text-black text-xs p-2 rounded whitespace-nowrap opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                    {text}
+                </div>
+            )}
         </div>
     )
 }
