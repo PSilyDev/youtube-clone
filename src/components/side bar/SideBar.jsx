827ebadb86392a798009e0e9ee41838fdfdec5f0
psilydev <codes.prakhar@gmail.com>
2024-09-03 17:23:37 +0530

    side bar done

--- src/components/side bar/SideBar.jsx
+++ src/components/side bar/SideBar.jsx
@@ -6,62 +6,66 @@ export const SideBar = ({open}) => { // always pass open as {open} bool and not 
     return(
         <div className="flex flex-col pt-8">            
             {/* main tiles */}
-            <button>
+            {/* updated */}
+            <button className="hover:bg-gray-200 rounded-xl">
                 <SideBarContainer icon={HOME.icon} text={HOME.name} open={open} />
             </button>
-            <button>
+            <button className="hover:bg-gray-200 rounded-xl">
                 <SideBarContainer icon={SHORTS.icon} text={SHORTS.name} open={open} />
             </button>
-            <button>
+            <button className="hover:bg-gray-200 rounded-xl">
                 <SideBarContainer icon={SUBSCRIPTION.icon} text={SUBSCRIPTION.name} open={open} />
             </button>
-            
+           
             {open && <hr className="self-center w-11/12 border-t border-gray-300 my-4" />}
 
             {/* you tiles */}
-            {open ? 
-            (<button>
+            {open ?
+            (<button className="hover:bg-gray-200 rounded-xl">
                 <div className= "mx-3 flex justify-start items-center h-11 rounded-xl hover:bg-gray-200">
                     <div className="font-semi text-lg ml-5">You &#62;</div>
                 </div>
-            </button>) : (<SideBarContainer icon={YOU.icon} text={YOU.name} open={open} />)}
+            </button>) :
+            (<button className="hover:bg-gray-200 rounded-xl">
+                <SideBarContainer icon={YOU.icon} text={YOU.name} open={open} />
+            </button>
+            )}
             {
-                open && 
+                open &&
                 (
                     <>
-                        <button>
+                        <button className="hover:bg-gray-200 rounded-xl">
                             <SideBarContainer icon={HISTORY.icon} text={HISTORY.name} open={open} />
                         </button>
-                        <button>
+                        <button className="hover:bg-gray-200 rounded-xl">
                             <SideBarContainer icon={PLAYLISTS.icon} text={PLAYLISTS.name} open={open} />
                         </button>
-                        <button>
+                        <button className="hover:bg-gray-200 rounded-xl">
                             <SideBarContainer icon={WATCHLATER.icon} text={WATCHLATER.name} open={open} />
                         </button>
-                        <button>
+                        <button className="hover:bg-gray-200 rounded-xl">
                             <SideBarContainer icon={LIKEDVIDEOS.icon} text={LIKEDVIDEOS.name} open={open} />
                         </button>
-                    
+                   
 
                         {open && <hr className="self-center w-11/12 border-t border-gray-300 my-4" open={open} />}
 
                         {/* settings */}
-                        <button>
+                        <button className="hover:bg-gray-200 rounded-xl">
                             <SideBarContainer icon={SETTINGS.icon} text={SETTINGS.name} open={open} />
                         </button>
-                        <button>
+                        <button className="hover:bg-gray-200 rounded-xl">
                             <SideBarContainer icon={REPORTHISTORY.icon} text={REPORTHISTORY.name} open={open} />
                         </button>
-                        <button>
+                        <button className="hover:bg-gray-200 rounded-xl">
                             <SideBarContainer icon={HELP.icon} text={HELP.name} open={open} />
                         </button>
-                        <button>
+                        <button className="hover:bg-gray-200 rounded-xl">
                             <SideBarContainer icon={SENDFEEDBACK.icon} text={SENDFEEDBACK.name} open={open} />
                         </button>
                     </>
                 )
             }
-            
         </div>
     )
 }
--- src/components/side bar/SideBarContainer.jsx
+++ src/components/side bar/SideBarContainer.jsx
@@ -1,15 +1,15 @@
 export const SideBarContainer = ({icon, text, open}) => {
     return(
-        <div className= {`relative flex items-center h-11 rounded-xl hover:bg-gray-200 mx-3 mt-2 ${open ? "justify-start" : "justify-center"} group`}>
+        <div className= {`relative flex items-center mx-3 ${open ? "justify-start h-12 mt-1" : "flex-col justify-center h-20 w-20 mx-0"} group`}>
+            {/* icon */}
             <div className={`flex items-center rounded-full h-8 w-8 p-1`}>{icon}</div>
-            {open?
-            (<div className="flex items-center ml-4 text-lg font-semi">{text}</div>)
-            : (<div></div>)}
-            {!open && (
-                <div className="absolute left-full ml-3 bg-white text-black text-xs p-2 rounded whitespace-nowrap opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-                    {text}
-                </div>
-            )}
+            {/* text */}
+            <div className={`flex items-center text-lg font-semi ${open ? " ml-4":"text-xs mt-1"}`}>{text}</div>
+            {/* tooltip */}
+            <div className={`absolute left-full bg-white text-black text-xs p-2 rounded whitespace-nowrap opacity-0 group-hover:opacity-100 transition-opacity duration-300 ${open ? "ml-5" : "ml-3"}`}>
+                {text}
+            </div>
+           
         </div>
     )
 }
