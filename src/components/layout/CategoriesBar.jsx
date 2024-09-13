6cd839e4e560a01e78186fc36a6bf267165b6947
PSilyDev <codes.prakhar@gmail.com>
2024-09-13 23:43:56 +0530

    categories bar ui update

--- src/components/layout/CategoriesBar.jsx
+++ src/components/layout/CategoriesBar.jsx
@@ -64,9 +64,9 @@ export const CategoriesBar = () => {
         //         <svg className="w-8 h-8 shadow-[0_0_30px_rgba(0,0,0,0.8)] rounded-full" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path fill="#403f3f" d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"/></svg>
         //     </button>
         // </div>
-        <div className="flex justify-center items-center bg-red-400 cursor-pointer m-16 rounded-md h-12 px-3 w-max">
+        <div className="flex items-center bg-red-400 cursor-pointer m-16 rounded-md h-12 px-3 max-w-screen-sm overflow-hidden">
             <svg className="w-8 h-8" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path fill="#403f3f" d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"/></svg>
-            <div className="flex flex-nowrap justify-start items-center">
+            <div className="flex flex-nowrap justify-start items-center overflow-x-scroll no-scrollbar">
             {
                 categories.map((value, index) => {
                     return (
