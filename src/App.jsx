c2822ff68a9e7a6512634340ab3e45c2becbf76e
PSilyDev <codes.prakhar@gmail.com>
2024-09-30 22:00:28 +0530

    accessToken initialized

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@ import { CategoriesBar } from "./components/layout/CategoriesBar";
 import { MainLayout } from "./components/layout/MainLayout";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { MainVideoGrid } from "./components/layout/MainVideoGrid";
+import { useSelector } from "react-redux";
 
 export default function App() {
+  const {accessToken, loading} = useSelector(state => state.auth);
   return (
     <Router>
       <Routes>
