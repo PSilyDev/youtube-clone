f5f5d7e794f612e822e153d491b0aae4d301eaab
PSilyDev <codes.prakhar@gmail.com>
2024-10-02 22:45:23 +0530

    firebase provider added

---
+++ src/redux/actions/auth.action.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+import auth from '../../firebase';
+
+// creating an action creator, action creator is a function that creates an action
+const login = () => async dispatch => {
+    try{
+
+    }
+    catch(error){
+
+    }
+}
