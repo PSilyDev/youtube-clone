31a7bc8e06b34c48d26bbd34dc1a6c94a6ef24d2
PSilyDev <codes.prakhar@gmail.com>
2024-10-03 22:46:45 +0530

    authorization action created

--- src/redux/actions/auth.action.js
+++ src/redux/actions/auth.action.js
@@ -1,10 +1,13 @@
-import firebase from 'firebase/app';
-import auth from '../../firebase';
+import firebase from 'firebase/compat/app';
+import auth from '../../firebase'
 
 // creating an action creator, action creator is a function that creates an action
-const login = () => async dispatch => {
+export const login = () => async dispatch => {
     try{
+        const provider = new firebase.auth.GoogleAuthProvider();
 
+        const res = await auth.signInWithPopup(provider);
+        console.log(res);
     }
     catch(error){
 
