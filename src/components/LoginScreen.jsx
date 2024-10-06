b06cec39daef091181474da7c2bdb211b35f404c
PSilyDev <codes.prakhar@gmail.com>
2024-10-06 22:45:30 +0530

    dispatch added

--- src/components/LoginScreen.jsx
+++ src/components/LoginScreen.jsx
@@ -1,11 +1,20 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { login } from "../redux/actions/auth.action";
 
 export const LoginScreen = () => {
+
+    const dispatch = useDispatch();
+
+    const handleLogin = () => {
+        dispatch(login());
+    }
+
     return(
         <div>
             <div>
                 <h2 className="bg-red-400">YouTube</h2>
-                <button className="bg-blue-400">Login with Google</button>
+                <button className="bg-blue-400" onClick={handleLogin}>Login with Google</button>
             </div>
         </div>
     )
