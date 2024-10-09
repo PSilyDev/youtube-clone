c9659c5d2f819a2dfc249b0aa5460c88fb9c289b
PSilyDev <codes.prakhar@gmail.com>
2024-10-09 20:22:19 +0530

    store update

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,19 +1,27 @@
-import { createStore, applyMiddleware } from 'redux';
-// import { composeWithDevTools } from '@redux-devtools/extension/lib/types/logOnly';
-import { composeWithDevTools } from '@redux-devtools/extension';
+// import { createStore, applyMiddleware } from 'redux';
+// // import { composeWithDevTools } from '@redux-devtools/extension/lib/types/logOnly';
+// import { composeWithDevTools } from '@redux-devtools/extension';
 
-import { thunk } from 'redux-thunk';
+// import { thunk } from 'redux-thunk';
 
-const initialState = {
-    name: "Prakhar",
+// const initialState = {
+//     name: "Prakhar",
     
-}
+// }
 
-const reducer = (initialState) => initialState;
+// const reducer = (initialState) => initialState;
 
-const store = createStore(
-    reducer, initialState,
-    composeWithDevTools(applyMiddleware(thunk))
-);
+// const store = createStore(
+//     reducer, initialState,
+//     composeWithDevTools(applyMiddleware(thunk))
+// );
 
-export default store;
+// export default store;
+
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import thunk from 'redux-thunk';
+import { authReducer } from './reducers/auth.reducer';
+
+const rootReducer = combineReducers({
+    auth: authReducer,
+})
