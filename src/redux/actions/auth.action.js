704d30c1299457c5b709a16823f0f6c7c0fab45e
PSilyDev <codes.prakhar@gmail.com>
2024-10-12 12:13:31 +0530

    auth action update

--- src/redux/actions/auth.action.js
+++ src/redux/actions/auth.action.js
@@ -1,15 +1,26 @@
 // import firebase from 'firebase/compat/app';
 import { signInWithPopup } from "firebase/auth";
 import { auth, googleProvider } from '../../firebase'
+import { LOGIN_REQUEST } from "../actionTypes";
 
 // creating an action creator, action creator is a function that creates an action
 export const login = () => async dispatch => {
     console.log('called')
     try{
         // const provider = new firebase.auth.GoogleAuthProvider();
+        dispatch({
+            type: LOGIN_REQUEST,
+
+        })
 
         const res = await signInWithPopup(auth, googleProvider);
         console.log(res);
+
+        const accessToken = res._tokenResponse.idToken;
+
+        const profile = {
+            name: res._tokenResponse.displayName;
+        }
     }
     catch(error){
         console.log('Error during google login : ', error);
